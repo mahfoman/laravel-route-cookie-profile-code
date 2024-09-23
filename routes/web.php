e8d0f1d7e5299dee62d7b4c7e4671b2b807f2744
Md Mahfoozur Rahman <mahfoozur.rahman.bd@gmail.com>
2024-09-24 00:37:03 +0700

    tweaked code

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/profile/{id}', [ProfileController::class, 'index']);
-Route::get('/get-cookie', [ProfileController::class, 'getCookieValue']);
